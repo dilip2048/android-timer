8b2d3e9217c031e5027d6476bbf276b0a5494391
Dilip Yadav <dilipptt@gmail.com>
2015-06-22 23:55:21 +0530

    Simple Android Timer

--- app/src/main/java/com/javacoodegeeks/android/androidtimerexample/MainActivity.java
+++ app/src/main/java/com/javacoodegeeks/android/androidtimerexample/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends Activity {
 
     private Button startButton;
     private Button pauseButton;
+    private Button resetButton;
+
 
     private TextView timerValue;
 
@@ -30,7 +32,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         timerValue = (TextView) findViewById(R.id.timerValue);
 
@@ -56,6 +58,28 @@ public class MainActivity extends Activity {
 
             }
         });
+        resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //resetButton =
+               // resetButton = SystemClock.;
+                //customHandler.removeCallbacks(updateTimerThread);
+
+                timeInMilliseconds = 0L;
+                timeSwapBuff = 0L;
+                updatedTime = 0L;
+                startTime=SystemClock.uptimeMillis();
+                //customHandler.postDelayed(updateTimerThread, 0);
+                int mins=0,secs=0,milliseconds=0;
+                timerValue.setText("" + mins + ":"
+                        + String.format("%02d", secs) + ":"
+                        + String.format("%03d", milliseconds));
+                customHandler.removeCallbacks(updateTimerThread);
+
+            }
+        });
+
 
     }
 
